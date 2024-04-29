71057a10d5958d1afdcee074827794a0e13d1478
Sakura286 <sakura286@outlook.com>
2024-04-30 01:21:18 +0800

    Rebase from 'upstream'

--- as2_behaviors/as2_behaviors_motion/takeoff_behavior/include/takeoff_behavior/takeoff_base.hpp
+++ as2_behaviors/as2_behaviors_motion/takeoff_behavior/include/takeoff_behavior/takeoff_base.hpp
@@ -37,8 +37,6 @@
 #ifndef TAKEOFF_BASE_HPP
 #define TAKEOFF_BASE_HPP
 
-#include <geometry_msgs/msg/pose_stamped.hpp>
-#include <geometry_msgs/msg/twist_stamped.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
 
 #include "as2_behavior/behavior_server.hpp"
@@ -47,6 +45,9 @@
 #include "as2_msgs/action/takeoff.hpp"
 #include "as2_msgs/msg/platform_info.hpp"
 #include "as2_msgs/msg/platform_status.hpp"
+
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/twist_stamped.hpp>
 
 namespace takeoff_base {
 
@@ -64,9 +65,9 @@ public:
   TakeoffBase(){};
   virtual ~TakeoffBase(){};
 
-  void initialize(as2::Node* node_ptr,
+  void initialize(as2::Node *node_ptr,
                   std::shared_ptr<as2::tf::TfHandler> tf_handler,
-                  takeoff_plugin_params& params) {
+                  takeoff_plugin_params &params) {
     node_ptr_             = node_ptr;
     tf_handler            = tf_handler;
     params_               = params;
@@ -74,8 +75,8 @@ public:
     this->ownInit();
   }
 
-  virtual void state_callback(geometry_msgs::msg::PoseStamped& pose_msg,
-                              geometry_msgs::msg::TwistStamped& twist_msg) {
+  virtual void state_callback(geometry_msgs::msg::PoseStamped &pose_msg,
+                              geometry_msgs::msg::TwistStamped &twist_msg) {
     actual_pose_ = pose_msg;
 
     feedback_.actual_takeoff_height = actual_pose_.pose.position.z;
@@ -107,15 +108,15 @@ public:
     return false;
   }
 
-  inline bool on_deactivate(const std::shared_ptr<std::string>& message) {
+  inline bool on_deactivate(const std::shared_ptr<std::string> &message) {
     return own_deactivate(message);
   }
 
-  inline bool on_pause(const std::shared_ptr<std::string>& message) { return own_pause(message); }
+  inline bool on_pause(const std::shared_ptr<std::string> &message) { return own_pause(message); }
 
-  inline bool on_resume(const std::shared_ptr<std::string>& message) { return own_resume(message); }
+  inline bool on_resume(const std::shared_ptr<std::string> &message) { return own_resume(message); }
 
-  void on_execution_end(const as2_behavior::ExecutionStatus& state) {
+  void on_execution_end(const as2_behavior::ExecutionStatus &state) {
     localization_flag_ = false;
     own_execution_end(state);
     return;
@@ -123,8 +124,8 @@ public:
 
   as2_behavior::ExecutionStatus on_run(
       const std::shared_ptr<const as2_msgs::action::Takeoff::Goal> goal,
-      std::shared_ptr<as2_msgs::action::Takeoff::Feedback>& feedback_msg,
-      std::shared_ptr<as2_msgs::action::Takeoff::Result>& result_msg) {
+      std::shared_ptr<as2_msgs::action::Takeoff::Feedback> &feedback_msg,
+      std::shared_ptr<as2_msgs::action::Takeoff::Result> &result_msg) {
     as2_behavior::ExecutionStatus status = own_run();
 
     feedback_msg = std::make_shared<as2_msgs::action::Takeoff::Feedback>(feedback_);
@@ -133,7 +134,7 @@ public:
   }
 
 private:
-  bool processGoal(as2_msgs::action::Takeoff::Goal& _goal) {
+  bool processGoal(as2_msgs::action::Takeoff::Goal &_goal) {
     if (!localization_flag_) {
       RCLCPP_ERROR(node_ptr_->get_logger(), "Behavior reject, there is no localization");
       return false;
@@ -149,27 +150,27 @@ private:
 protected:
   virtual void ownInit(){};
 
-  virtual bool own_activate(as2_msgs::action::Takeoff::Goal& goal) = 0;
+  virtual bool own_activate(as2_msgs::action::Takeoff::Goal &goal) = 0;
 
-  virtual bool own_modify(as2_msgs::action::Takeoff::Goal& goal) {
+  virtual bool own_modify(as2_msgs::action::Takeoff::Goal &goal) {
     RCLCPP_INFO(node_ptr_->get_logger(), "Takeoff can not be modified, not implemented");
     return false;
   }
 
-  virtual bool own_deactivate(const std::shared_ptr<std::string>& message) = 0;
+  virtual bool own_deactivate(const std::shared_ptr<std::string> &message) = 0;
 
-  virtual bool own_pause(const std::shared_ptr<std::string>& message) {
+  virtual bool own_pause(const std::shared_ptr<std::string> &message) {
     RCLCPP_INFO(node_ptr_->get_logger(),
                 "Takeoff can not be paused, not implemented, try to cancel it");
     return false;
   }
 
-  virtual bool own_resume(const std::shared_ptr<std::string>& message) {
+  virtual bool own_resume(const std::shared_ptr<std::string> &message) {
     RCLCPP_INFO(node_ptr_->get_logger(), "Takeoff can not be resumed, not implemented");
     return false;
   }
 
-  virtual void own_execution_end(const as2_behavior::ExecutionStatus& state) = 0;
+  virtual void own_execution_end(const as2_behavior::ExecutionStatus &state) = 0;
   virtual as2_behavior::ExecutionStatus own_run()                            = 0;
 
   inline void sendHover() {
@@ -178,7 +179,7 @@ protected:
   };
 
 protected:
-  as2::Node* node_ptr_;
+  as2::Node *node_ptr_;
   std::shared_ptr<as2::tf::TfHandler> tf_handler = nullptr;
 
   as2_msgs::action::Takeoff::Goal goal_;
